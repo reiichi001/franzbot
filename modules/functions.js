5e0f49e3c3d2eba00e355bb944377d9ac4d1d074
Robert Baker <reiichi001@gmail.com>
2020-08-11 17:11:59 -0700

    Remove unused function

--- modules/functions.js
+++ modules/functions.js
@@ -93,32 +93,6 @@ module.exports = (client) =>
         }
     };
 
-
-    /*
-    MESSAGE CLEAN FUNCTION
-    "Clean" removes @everyone pings, as well as tokens, and makes code blocks
-    escaped so they're shown more easily. As a bonus it resolves promises
-    and stringifies objects!
-    This is mostly only used by the Eval and Exec commands.
-    */
-    client.clean = async (client, text) =>
-    {
-        if (text && text.constructor.name == "Promise")
-            text = await text;
-        if (typeof evaled !== "string")
-            text = require("util").inspect(text,
-            {
-                depth: 1
-            });
-
-        text = text
-            .replace(/`/g, "`" + String.fromCharCode(8203))
-            .replace(/@/g, "@" + String.fromCharCode(8203))
-            .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
-
-        return text;
-    };
-
     client.loadCommand = (commandName) =>
     {
         try
