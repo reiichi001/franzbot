027f09798fbbb8dbae998e6ebf6e0b07c5ca4b34
Robert Baker <reiichi001@gmail.com>
2020-08-11 21:24:52 -0700

    Try to add eslint

--- events/message.js
+++ events/message.js
@@ -1,25 +1,75 @@
-/*
-module.exports = (client, message) => {
-  // Ignore all bots
-  if (message.author.bot) return;
+function checkTheMessage(message, forbidAny, forbidCount, negateBadWords, forbiddenMinCount, adjustedMinCount, ignoredRoles, replyMessage) {
+	if (message.member.roles.cache.some(r => ignoredRoles.includes(r.name))) {
+		return;
+	}
 
-  // Ignore messages not starting with the prefix (in config.json)
-  if (message.content.indexOf(client.config.prefix) !== 0) return;
+	let offsetWeight = 0;
+	let hasForbidAny = false;
+	let forbidCountQuantity = 0;
+	const forbidden = new Set();
 
-  // Our standard argument/command name definition.
-  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+	if (!forbidAny) {
+		forbidAny = [];
+	}
+	if (!Array.isArray(forbidAny)) {
+		forbidAny = [forbidAny];
+	}
+	if (!forbidCount) {
+		forbidCount = [];
+	}
+	if (!Array.isArray(forbidCount)) {
+		forbidCount = [forbidCount];
+	}
+	if (!negateBadWords) {
+		negateBadWords = [];
+	}
+	if (!Array.isArray(negateBadWords)) {
+		negateBadWords = [negateBadWords];
+	}
 
-  // Grab the command data from the client.commands Enmap
-  const cmd = client.commands.get(command);
+	for (const forbid of forbidAny) {
+		const results = message.content.match(forbid);
+		if (results) {
+			hasForbidAny = true;
+			const noun = `forbidden word${results.length == 1 ? '' : 's'}`;
+			console.log(`Matched ${results.length} ${noun}: ${results.join(',')}`);
+			for (const item of results) {
+				forbidden.add(item);
+			}
+		}
+	}
+	for (const forbid of forbidCount) {
+		const results = message.content.match(forbid);
+		if (results) {
+			forbidCountQuantity += results.length;
+			const noun = `bad word${results.length == 1 ? '' : 's'}`;
+			console.log(`Matched ${results.length} ${noun}: ${results.join(',')}`);
+			for (const item of results) {
+				forbidden.add(item);
+			}
+		}
+	}
 
-  // If that command doesn't exist, silently exit and do nothing
-  if (!cmd) return;
+	for (const permit of negateBadWords) {
+		const results = message.content.match(permit);
+		if (results) {
+			offsetWeight = results.length;
+			const noun = `good word${results.length == 1 ? '' : 's'}`;
+			console.log(`Matched ${results.length} ${noun}: ${results.join(',')}`);
+		}
+	}
 
-  // Run the command
-  cmd.run(client, message, args);
-};
-*/
+	console.log(`Bad wordset: ${forbidden.size} Goodwords: ${offsetWeight}`);
+
+	const adjustedWordWeight = forbidden.size - offsetWeight;
+	if (replyMessage
+		&& hasForbidAny
+		&& forbidCountQuantity >= forbiddenMinCount
+		&& adjustedWordWeight >= adjustedMinCount
+	) {
+		message.reply(replyMessage);
+	}
+}
 
 // The MESSAGE event runs anytime a message is received
 // Note that due to the binding of client to every event, every event
@@ -118,10 +168,10 @@ module.exports = async (client, message) => {
 			},
 		};
 	}
-	
+
 	checkTheMessage(message, forbidAny, forbidCount, negateBadWords, forbiddenMinCount, adjustedMinCount, ignoredRoles, replyMessage);
-	
-	//bandaid, clear all the important variables
+
+	// bandaid, clear all the important variables
 	forbidAny = [];
 	forbidCount = [];
 	negateBadWords = [];
@@ -151,10 +201,10 @@ module.exports = async (client, message) => {
 			},
 		};
 	}
-	
+
 	checkTheMessage(message, forbidAny, forbidCount, negateBadWords, forbiddenMinCount, adjustedMinCount, ignoredRoles, replyMessage);
-	
-	//bandaid, clear all the important variables
+
+	// bandaid, clear all the important variables
 	forbidAny = [];
 	forbidCount = [];
 	negateBadWords = [];
@@ -192,15 +242,14 @@ module.exports = async (client, message) => {
 			},
 		};
 	}
-	
+
 	checkTheMessage(message, forbidAny, forbidCount, negateBadWords, forbiddenMinCount, adjustedMinCount, ignoredRoles, replyMessage);
-	
-	//bandaid, clear all the important variables
+
+	// bandaid, clear all the important variables
 	forbidAny = [];
 	forbidCount = [];
 	negateBadWords = [];
 
-	
 
 	// End of PrincessRTFM's rewrite
 
@@ -252,77 +301,3 @@ module.exports = async (client, message) => {
 	}
 	cmd.run(client, message, args);
 };
-
-function checkTheMessage(message, forbidAny, forbidCount, negateBadWords, forbiddenMinCount, adjustedMinCount, ignoredRoles, replyMessage) {
-	if (message.member.roles.cache.some(r => ignoredRoles.includes(r.name))) {
-		return;
-	}
-
-	let offsetWeight = 0;
-	let hasForbidAny = false;
-	let forbidCountQuantity = 0;
-	const forbidden = new Set();
-
-	if (!forbidAny) {
-		forbidAny = [];
-	}
-	if (!Array.isArray(forbidAny)) {
-		forbidAny = [forbidAny];
-	}
-	if (!forbidCount) {
-		forbidCount = [];
-	}
-	if (!Array.isArray(forbidCount)) {
-		forbidCount = [forbidCount];
-	}
-	if (!negateBadWords) {
-		negateBadWords = [];
-	}
-	if (!Array.isArray(negateBadWords)) {
-		negateBadWords = [negateBadWords];
-	}
-
-	for (const forbid of forbidAny) {
-		const results = message.content.match(forbid);
-		if (results) {
-			hasForbidAny = true;
-			const noun = `forbidden word${results.length == 1 ? '' : 's'}`;
-			console.log(`Matched ${results.length} ${noun}: ${results.join(',')}`);
-			for (const item of results) {
-				forbidden.add(item);
-			}
-		}
-	}
-	for (const forbid of forbidCount) {
-		const results = message.content.match(forbid);
-		if (results) {
-			forbidCountQuantity += results.length;
-			const noun = `bad word${results.length == 1 ? '' : 's'}`;
-			console.log(`Matched ${results.length} ${noun}: ${results.join(',')}`);
-			for (const item of results) {
-				forbidden.add(item);
-			}
-		}
-	}
-
-	for (const permit of negateBadWords) {
-		const results = message.content.match(permit);
-		if (results) {
-			offsetWeight = results.length;
-			const noun = `good word${results.length == 1 ? '' : 's'}`;
-			console.log(`Matched ${results.length} ${noun}: ${results.join(',')}`);
-		}
-	}
-
-	console.log(`Bad wordset: ${forbidden.size} Goodwords: ${offsetWeight}`);
-
-	const adjustedWordWeight = forbidden.size - offsetWeight;
-	if (replyMessage
-		&& hasForbidAny
-		&& forbidCountQuantity >= forbiddenMinCount
-		&& adjustedWordWeight >= adjustedMinCount
-	) {
-		message.reply(replyMessage);
-	}
-		
-}
