5b55d1f34c0c203065401cac1299769bb3dd9f97
Amenne <46428595+AmenneHolelane@users.noreply.github.com>
2021-04-04 23:48:12 -0600

    added one q

--- config/581875019861328007/faq.js
+++ config/581875019861328007/faq.js
@@ -157,7 +157,7 @@ exports.run = async (client, message, args) => {
 					"title": `OpCodes need to be manually updated each patch.`,
 					"description": `OpCode information is updated separately from Dalamud. Chances are you just need to relaunch if `
 					+ `they've been updated since your last launch. \n\n More Info `
-					+ ` [HERE](https://github.com/goatcorp/faq/blob/main/xl_troubleshooting.md#how-do-i-fix-plugins-that-rely-on-dalamud-provided-opcodes)`,
+					+ ` [HERE](https://github.com/goatcorp/faq/blob/main/xl_troubleshooting.md#q-how-do-i-fix-plugins-that-rely-on-dalamud-provided-opcodes)`,
 					"color": client.config.EMBED_NORMAL_COLOR,
 					"footer": {
 						"text": client.config.FRANZBOT_VERSION,
