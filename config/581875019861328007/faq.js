52bfcc4e22e5419f16989642729b409e03ab1e72
Robert Baker <reiichi001@gmail.com>
2021-05-13 13:14:11 -0700

    PaissaHouse has been updated
    Remove the bot faq saying it's old.

--- config/581875019861328007/faq.js
+++ config/581875019861328007/faq.js
@@ -54,8 +54,6 @@ exports.run = async (client, message, args) => {
 		case "dns": args[0] = "vpn"; break;
 		case "oldplugin": args[0] = "oldplugins"; break;
 		case "adventurerinneed": args[0] = "oldplugins"; break;
-		case "autosweep": args[0] = "oldplugins"; break;
-		case "paissahouse": args[0] = "oldplugins"; break;
 		case "chatextender": args[0] = "oldplugins"; break;
 		case "deepdungeondex ": args[0] = "oldplugins"; break;
 		case "voidlist": args[0] = "oldplugins"; break;
