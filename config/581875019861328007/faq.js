a86b7f595a4f107322772b69304bac04b605ead1
Robert Baker <reiichi001@gmail.com>
2021-05-24 14:41:22 -0700

    update the new patch FAQ

--- config/581875019861328007/faq.js
+++ config/581875019861328007/faq.js
@@ -443,7 +443,8 @@ exports.run = async (client, message, args) => {
 			responses.push({
 				"embed": {
 					"title": `Please wait for Dalamud and Plugin updates after a patch`,
-					"description": `[Again, please see the faq](https://goatcorp.github.io/faq/xl_troubleshooting#q-do-not-expect-xldalamudplugin-updates-on-patch-day-releases)`,
+					"description": `XIVLauncher should work just fine after a patch.\n\nDalamud and plugins will need to be updated.\n\nMore Info `
+						+ `[HERE](https://goatcorp.github.io/faq/xl_troubleshooting#q-do-not-expect-dalamud-andor-plugins-to-work-on-updatespatch-day-releases)`,
 					"color": client.config.EMBED_NORMAL_COLOR,
 					"footer": {
 						"text": client.config.FRANZBOT_VERSION,
