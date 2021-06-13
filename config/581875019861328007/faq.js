088fd54b7a2a0bc9150f2f584f8c42e1e612087e
Robert Baker <reiichi001@gmail.com>
2021-06-12 21:25:47 -0700

    fix some typos

--- config/581875019861328007/faq.js
+++ config/581875019861328007/faq.js
@@ -246,10 +246,10 @@ exports.run = async (client, message, args) => {
 		case "repair":
 			responses.push({
 				"embed": {
-					"title": `Can I repair my FFXIV installion?`,
+					"title": `Can I repair my FFXIV installation?`,
 					"description": `Yes, in the sense that you can reinstall the game. `
 						+ `But it can be reinstalled selectively to help cut down on download/install time if needed.\n\n`
-						+ `More Info: [HERE](https://goatcorp.github.io/faq/xl_troubleshooting#q-can-i-repair-my-ffxiv-installion)`,
+						+ `More Info: [HERE](https://goatcorp.github.io/faq/xl_troubleshooting#q-can-i-repair-my-ffxiv-installation)`,
 					"color": client.config.EMBED_NORMAL_COLOR,
 					"footer": {
 						"text": client.config.FRANZBOT_VERSION,
@@ -526,7 +526,7 @@ exports.run = async (client, message, args) => {
 				"embed": {
 					"title": `Injection delay for RivaTuner/RTSS`,
 					"description": `Please follow steps listed in the FAQ `
-						+ `[HERE](https://goatcorp.github.io/faq/xl_troubleshooting#q-how-to-set-an-an-injection-delay-in-rivatunerrtss)`,
+						+ `[HERE](https://goatcorp.github.io/faq/xl_troubleshooting#q-how-to-set-an-injection-delay-in-rivatunerrtss)`,
 					"color": client.config.EMBED_NORMAL_COLOR,
 					"footer": {
 						"text": client.config.FRANZBOT_VERSION,
