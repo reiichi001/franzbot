cd2f714aade3e33e4241316f906fd3e7dcb6b4a5
Robert Baker <reiichi001@gmail.com>
2021-06-12 22:16:39 -0700

    expand security FAQ

--- config/581875019861328007/faq.js
+++ config/581875019861328007/faq.js
@@ -190,8 +190,14 @@ exports.run = async (client, message, args) => {
 			responses.push({
 				"embed": {
 					"title": `Are XIVLauncher, Dalamud, and Dalamud Plugins safe to use?`,
-					"description": `Yes! We've taken steps to ensure that using XIVLauncher, `
-						+ `Dalamud, and **officially supported** Dalamud Plugins are safe to use.`
+					"description": `For the most part, yes, this should be safe to use. We've taken steps to `
+						+ `ensure that using XIVLauncher, Dalamud, and **officially supported** `
+						+ `Dalamud Plugins should not result in invalid or unexpected behavior. `
+						+ `Like other game modifictions, you shouldn't talk about using it in-game and should exercise `
+						+ `caution over which plugins you use. XIVLauncher and Dalamud are not designed `
+						+ `for game exploitation, automation, or botting.`
+						+ `\n\nTo the best of our knowledge, SE does not check for or prevent the usage of `
+						+ `third party tools, but they are against the terms of service.`
 						+ `\n\nPlease note that we cannot vouch for the safety of third party plugins.`
 						+ `\n\nIf you'd like more information, please see the <#585958820061249537> channel and our FAQ post `
 						+ `[HERE](https://goatcorp.github.io/faq/xl_troubleshooting#q-are-xivlauncher-dalamud-and-dalamud-plugins-safe-to-use)`,
