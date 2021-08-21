d2d13c9da88b52e84c9d85a4e4e4a6ec7a81b10f
Robert Baker <reiichi001@gmail.com>
2021-08-21 13:11:24 -0700

    update wording

--- config/581875019861328007/faq.js
+++ config/581875019861328007/faq.js
@@ -575,7 +575,7 @@ exports.run = async (client, message, args) => {
 						+ `between different locations as long as your computer has all redistributables needed to run it.\n\n`
 						+ `XIVLauncher on the other hand is *technically* portable, but we recommend only copying plugin configuration `
 						+ `accross different computers as the launcher/dalamud settings should stay machine-specific and you should `
-						+ `**NEVER** copy your installed plugins. (Users who know enough should know better)\n\n`
+						+ `**NOT** copy your installed plugins. (Users who know enough should know better)\n\n`
 						+ `The Linux FAQ post for this can be found `
 						+ `[HERE](https://goatcorp.github.io/faq/xl_troubleshooting#q-how-do-i-migrate-ffxiv-andor-xivlauncher-files-from-an-old-wine-prefix-to-a-new-one-linux)`
 						+ `\nThe Windows FAQ post for this can be found `
