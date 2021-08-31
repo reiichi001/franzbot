d845f9d6083aa8596b4b9f9498a649826b8c83be
Robert Baker <reiichi001@gmail.com>
2021-08-31 09:04:08 -0700

    Add FAQ for Plugin Testing

--- config/581875019861328007/faq.js
+++ config/581875019861328007/faq.js
@@ -479,6 +479,24 @@ exports.run = async (client, message, args) => {
 				},
 			});
 			break;
+		case "plugintesting":
+			responses.push({
+				title: `How to enable Plugin Testing`,
+				description: `General "how to enable plugin test builds" steps:\n`
+					+ `1. Type \`/xlsettings\` in game.\n`
+					+ `2. Go to the \`Experimental\` tab\n`
+					+ `3. Click the checkbox for \`Get plugin testing builds\`\n`
+					+ `4. Save / Save and Close\n`
+					+ `\n`
+					+ `**Please note that testing plugins can/will have bugs and may change dramatically before final release. Especially on newer plugins, they could crash your game. `
+					+ `For troubleshooting, please keep questions/comments/issues in the <#719513457988337724> channel.**`
+					+ `[HERE](https://goatcorp.github.io/faq/dalamud_troubleshooting#q-how-do-i-enable-plugin-test-builds)`,
+				color: client.config.EMBED_NORMAL_COLOR,
+				footer: {
+					"text": client.config.FRANZBOT_VERSION,
+				},
+			});
+			break;
 		case "vpn":
 			responses.push({
 				title: `SE's routing is shit. Use a VPN`,
