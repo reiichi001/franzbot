1550e25c1d4903c5a279811b8bf80ad079020177
Robert Baker <reiichi001@gmail.com>
2021-10-09 13:15:06 -0700

    Add the most important FAQ command ever
    Also update the integrity FAQ slightly.

--- config/581875019861328007/faqs/integrity.js
+++ config/581875019861328007/faqs/integrity.js
@@ -3,7 +3,8 @@ exports.answer = async client => ({
 	title: `XIVLauncher integrity check`,
 	description: `The integrity check feature supports up to patch 5.45 Hotfix.\n\n`
 		+ `To run the check, open XIVLauncher, click the settings cog ⚙️, and select the Game tab. It will have a button.\n\n`
-		+ `__If your client has been modded via TexTools, the integrity check will fail.__`,
+		+ `You'll find the integrity report in your \`%appdata%\\xivlauncher\` folder. Please upload it for review.`
+		+ `__If your client has been modded via TexTools, the integrity check will always fail.__`,
 	image: {
 		"url": "https://cdn.discordapp.com/attachments/687530726756712478/828741457862197278/HvFDn.png",
 	},
