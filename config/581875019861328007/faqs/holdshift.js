f15ef62be9391b71ee739a81dc27dab4e4e987c0
Robert Baker <reiichi001@gmail.com>
2021-10-09 13:16:54 -0700

    fix holdshift formatting

--- config/581875019861328007/faqs/holdshift.js
+++ config/581875019861328007/faqs/holdshift.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 exports.answer = async client => ({
 	title: `How to disable autologin`,
-	description: `Keep the shift key held down while you open XIVLauncher. Please keep it held down as the program opens.`
+	description: `Keep the shift key held down while you open XIVLauncher. Please keep it held down as the program opens.\n\n`
 		+ `__You should have also gotten a popup telling you this when you first enabled it.__`,
 	color: client.config.EMBED_NORMAL_COLOR,
 	footer: {
