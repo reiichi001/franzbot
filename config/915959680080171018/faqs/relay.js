f21cbfc5c6595e176ff341eb94235ebadf849b0b
Franz Renatus <reiichi001@gmail.com>
2022-01-10 00:16:53 -0800

    fix relay faq

--- config/915959680080171018/faqs/relay.js
+++ config/915959680080171018/faqs/relay.js
@@ -1,7 +1,7 @@
 exports.answer = async client => ({
 	title: `Is relay turned on?`,
 	description: `XIV on Mac application does not support iCloud Relay. If you are getting errors installing`
-		+ `or patching errors, please turn off iCloud relay. Restart your Mac and relaunch our application afterwards.`
+		+ `or patching errors, please turn off iCloud relay. Restart your Mac and relaunch our application afterwards.`,
 	color: client.config.EMBED_NORMAL_COLOR,
 	footer: {
 		"text": client.config.FRANZBOT_VERSION,
