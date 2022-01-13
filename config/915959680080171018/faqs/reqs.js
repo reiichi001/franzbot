144c2ea3a3f65025849562794c7d02970ae889d3
Seathasky <seathasky@gmail.com>
2022-01-13 07:27:59 -0500

    Update reqs (Mac)
    New reqs included.

--- config/915959680080171018/faqs/reqs.js
+++ config/915959680080171018/faqs/reqs.js
@@ -1,6 +1,6 @@
 exports.answer = async client => ({
 	title: `XIV on Mac Application Requirements`,
-	description: `The XIV on Mac Application currently requires macOS Big Sur 11.6+ or higher.`
+	description: `The XIV on Mac Application currently requires macOS Catalina 10.15.7+ or higher.`
 		+ ``
 		+ `\n\nPlease visit our website to see additional hardware reqs & fps estimates, see `
 		+ `[HERE](https://www.xivmac.com/faq#q-xiv-on-mac-app-reqs)`,
