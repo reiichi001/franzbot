bdf5a5e209090aee784bd368c77fbd1d336411e4
Robert Baker <reiichi001@gmail.com>
2022-02-12 21:32:29 -0800

    Minor reshade faq adjustment

--- config/581875019861328007/faqs/reshade.js
+++ config/581875019861328007/faqs/reshade.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 exports.answer = async client => ({
-	title: `Reshade/Gshade/Stormshade/etc not working?`,
+	title: `Reshade/Gshade/etc not working or shaders being applied on plugin windows?`,
 	description: `Please check the faq post `
 		+ `[HERE](https://goatcorp.github.io/faq/dalamud_troubleshooting#q-reshade-and-its-variants-dont-work-or-dalamud-ui-fails)`,
 	color: client.config.EMBED_NORMAL_COLOR,
