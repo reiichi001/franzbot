a8340ac6fbf45b054b672de3b157a700a7e73f24
Franz Renatus <reiichi001@gmail.com>
2022-03-15 10:18:28 -0700

    Create reqs faq in testing server

---
+++ config/485251347852165120/faqs/reqs.js
@@ -0,0 +1,31 @@
+exports.answer = async client => ({
+	title: `XIV on Mac Application Requirements`,
+	description: `The XIV on Mac Application currently requires macOS Catalina 10.15 or later, although it is recommended to be on macOS Montery 12.0 or later for the best experience. Please check out <https://www.xivmac.com/compatibility-database> for more information.`
+		+ ``
+		+ `\n\nPlease visit our website to see additional hardware reqs & fps estimates, see `
+		+ `[HERE](https://www.xivmac.com/faq#q-xiv-on-mac-app-reqs)`,
+	fields: [
+		{
+			"name": "Intel Macs",
+			"value: "AMD Radeon GPUs are required. Intel graphics cannot be used.",
+		},
+		{
+			"name" "Apple Silicon"
+			"value": "M1 or better"
+		},
+	]
+	color: client.config.EMBED_NORMAL_COLOR,
+	footer: {
+		"text": client.config.FRANZBOT_VERSION,
+	},
+});
+
+exports.info = {
+	name: "reqs",
+	category: "help",
+	aliases: [
+		"reqs",
+		"req",
+		"requirements",
+	],
+};
