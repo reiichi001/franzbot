c8febbf2909e10779079f17335cd2fc9f137243a
Zoey Cluff <matada89@gmail.com>
2022-03-16 13:44:29 -0500

    added more detailed instructions to get to prefix folder
    Added detailed info to get to the right Application Support (~Application Support instead of /Application Support

--- config/915959680080171018/faqs/maclogd.js
+++ config/915959680080171018/faqs/maclogd.js
@@ -4,7 +4,8 @@ exports.answer = async client => ({
 		+ `\`~/Library/Application Support/XIV on Mac/game/drive_c \``
 		+ ` in this channel, so we can look into the problem!`
 		+ `\n\nIt's best to just upload/attach the file if you can!\n\n`
-		+ `__Please upload the file directly. Even if you have Nitro, please make sure it's under 5.0 MB.__`,
+		+ `__Please upload the file directly. Even if you have Nitro, please make sure it's under 5.0 MB.__`
+		+ `\n\nUse Finder's 'Go to Folder...' option in the Go menu or SHIFT+CMD+G and paste the path above in!`,
 	color: client.config.EMBED_NORMAL_COLOR,
 	image: {
 		"url": client.config.FINDERGOTOSCREENSHOT,
--- config/915959680080171018/faqs/prefix.js
+++ config/915959680080171018/faqs/prefix.js
@@ -3,7 +3,8 @@ exports.answer = async client => ({
 	description: `The XIV on Mac prefix is a folder that contains important WINE configurations and system data for XIV on Mac App.`
 		+ `\n\nThe XIV on Mac application data folder or "Prefix" is located at \`~/Library/Application Support/XIV on Mac\``
 		+ `\n\nFor more information, see `
-		+ `[HERE](https://www.xivmac.com/faq#q-prefix-folder?)`,
+		+ `[HERE](https://www.xivmac.com/faq#q-prefix-folder?)`
+		+ `\n\nUse Finder's 'Go to Folder...' option in the Go menu or SHIFT+CMD+G and paste the path above in!`,
 	color: client.config.EMBED_NORMAL_COLOR,
 	image: {
 		"url": client.config.FINDERGOTOSCREENSHOT,
