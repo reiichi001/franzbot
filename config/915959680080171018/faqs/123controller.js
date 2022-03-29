e416ff9d8cb33996da77dbdeaaffde74b2272806
Zoey Cluff <matada89@gmail.com>
2022-03-28 19:46:53 -0500

    Added franzbot command about the 12.3 controller regression

---
+++ config/915959680080171018/faqs/123controller.js
@@ -0,0 +1,21 @@
+exports.answer = async client => ({
+	title: `Updated to macOS 12.3 and now your controller is acting weird?`,
+	description: `Due to a regression in macOS 12.3 controllers may not function as expected, or not be useable at all. \n `
+		+ `This is due to a bug with macOS 12.3 and not with XIV on Mac. You can try running it plugged in (no bluetooth), or rebooting \n `
+		+ `your computer as this works for some players. Unfortunately we have to wait for a software update from Apple to resolve this`,
+	color: client.config.EMBED_NORMAL_COLOR,
+	footer: {
+		"text": client.config.FRANZBOT_VERSION,
+	},
+});
+
+exports.info = {
+	name: "controllerRegression",
+	category: "info",
+	aliases: [
+		"controller",
+		"12.3Controller",
+		"PS4",
+		"Xboxone",
+	],
+};
