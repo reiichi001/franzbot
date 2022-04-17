154bb7a8553260d32783454fb58265bf262a2d6e
Zoey Cluff <matada89@gmail.com>
2022-04-16 21:21:18 -0500

    fix lint

--- config/915959680080171018/faqs/fullscreen.js
+++ config/915959680080171018/faqs/fullscreen.js
@@ -2,7 +2,7 @@
 exports.answer = async client => ({
 	title: `Can't save graphic setting changes? Can't alt-tab? Switch to borderless window!`,
 	description: `Fullscreen causes random problems with saving changed graphics settings \n `
-		+ 'or breaking alt-tab \n `
+		+ `or breaking alt-tab \n `
 		+ `set the game to borderless window and they'll be resolved!`,
 	color: client.config.EMBED_NORMAL_COLOR,
 	footer: {
