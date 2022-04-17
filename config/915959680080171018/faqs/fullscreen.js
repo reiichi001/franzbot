40bcc507c4e2ea2d8ddf213319533bdcda3bc7c6
Zoey Cluff <matada89@gmail.com>
2022-04-16 21:19:41 -0500

    add an entry for using borderless window instead of full screen

---
+++ config/915959680080171018/faqs/fullscreen.js
@@ -0,0 +1,22 @@
+/* eslint-disable max-len */
+exports.answer = async client => ({
+	title: `Can't save graphic setting changes? Can't alt-tab? Switch to borderless window!`,
+	description: `Fullscreen causes random problems with saving changed graphics settings \n `
+		+ 'or breaking alt-tab \n `
+		+ `set the game to borderless window and they'll be resolved!`,
+	color: client.config.EMBED_NORMAL_COLOR,
+	footer: {
+		"text": client.config.FRANZBOT_VERSION,
+	},
+});
+
+exports.info = {
+	name: "fullscreen",
+	category: "info",
+	aliases: [
+		"fullscreen",
+		"borderless",
+		"graphicssaved",
+
+	],
+};
