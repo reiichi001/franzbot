693c1a389a1f755245d761ca8c65c20ef40db1d5
Zoey Cluff <matada89@gmail.com>
2022-04-24 18:16:34 -0500

    add FAQ about denying documents permissions

---
+++ config/915959680080171018/faqs/permissions.js
@@ -0,0 +1,24 @@
+exports.answer = async client => ({
+	title: `Can't find character config data? We need permissions!`,
+	description: 'In Security and privacy in System preferences, grant XIV on Mac", \n'
+    + 'permissions to your documents folder. This is what happens when you press \n'
+		+ "no on the dialogue asking for permissons",
+
+	color: client.config.EMBED_NORMAL_COLOR,
+	footer: {
+		"text": client.config.FRANZBOT_VERSION
+    },
+    "image": {
+      "url": "https://i.imgur.com/GaKCkJJ.png",
+    },
+  });
+
+exports.info = {
+	name: "permissions",
+	category: "help",
+	aliases: [
+		"permissions",
+		"cantfindconfig",
+
+	],
+};
