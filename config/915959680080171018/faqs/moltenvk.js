2fdf8f09213880762ce1b47e16a1e28720b14e1e
Zoey Cluff <matada89@gmail.com>
2022-05-17 22:57:04 -0500

    add an entry for modern moltenVK fixing crashing in Azys Lla

---
+++ config/915959680080171018/faqs/moltenvk.js
@@ -0,0 +1,23 @@
+exports.answer = async client => ({
+	"title": `Game Crashing in Azys Lla? Modern MoltenVK is your solution! `,
+	"description": "In XIV on Mac's preferences, under Graphics check 'Modern MoltenVK'to aleviate the crashing.  \n"
+    + 'please note that this can introduce some lighting issues, so you may want to uncheck it when you are done in the zone.',
+
+	"color": client.config.EMBED_NORMAL_COLOR,
+	"footer": {
+		"text": client.config.FRANZBOT_VERSION,
+	},
+	"image": {
+		"url": "https://i.imgur.com/I5Lo0P2.png",
+	},
+});
+
+exports.info = {
+	name: "moltenvk",
+	category: "help",
+	aliases: [
+		"modern",
+		"azys",
+
+	],
+};
