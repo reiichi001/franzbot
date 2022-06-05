73eedca07852f6b7fd5f516a37dc06825d71edf9
Robert Baker <reiichi001@gmail.com>
2022-06-05 14:57:20 -0700

    Add new textools faq and serenity guide faq for Penumbra

---
+++ config/884363610640498698/faqs/serenityguide.js
@@ -0,0 +1,24 @@
+/* eslint-disable max-len */
+exports.answer = async client => ({
+	title: `Please review the guide!`,
+	description: `A good starting place for information is the Guide to Penumbra made by <@858401899439194133>. \n\n`
+		+ `It has easily digestible information for everything from how to install Penumbra, to collections, to material assignment, and more. `
+		+ `If you have additional questions after reviewing the guide, feel free to ask for more assistance or clarification on the guide. \n\n`
+		+ `If you have any feedback for additional content for the guide, please DM Serenity directly.\n\n`
+		+ `**Guide URL:** <https://penumbra.ju.mp/>`,
+	color: client.config.EMBED_NORMAL_COLOR,
+	footer: {
+		"text": client.config.FRANZBOT_VERSION,
+	},
+});
+
+exports.info = {
+	name: "serenityguide",
+	category: "help",
+	aliases: [
+		"reni",
+		"reniguide",
+		"penumbraguide",
+		"justreadthispleasebeforeasking",
+	],
+};
