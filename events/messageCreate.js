edaedcc7813ec5998b85da5bbb835a086b8bcc9b
Robert Baker <reiichi001@gmail.com>
2022-06-10 11:43:12 -0700

    fix typo

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -223,10 +223,10 @@ module.exports = async (client, message) => {
 		// some debugging
 		let customChannel = null;
 		if (message.guild.id === client.config.GUILDID_GOAT) {
-			await client.channels.fetch(client.config.CHANNELID_RELAY_GOAT);
+			customChannel = await client.channels.fetch(client.config.CHANNELID_RELAY_GOAT);
 		}
 		if (message.guild.id === client.config.GUILDID_XIVONMAC) {
-			await client.channels.fetch(client.config.CHANNELID_RELAY_XIVONMAC);
+			customChannel = await client.channels.fetch(client.config.CHANNELID_RELAY_XIVONMAC);
 		}
 		if (client.config.DEBUGMODE) {
 			customChannel = await client.channels.fetch(client.config.CHANNELID_RELAY_TEST);
