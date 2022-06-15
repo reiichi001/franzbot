beba3ce28b8f2bd934d15e978ac5c21671df5148
Seathasky <seathasky@gmail.com>
2022-06-15 06:29:43 -0400

    update prefix faq
    "old" is incorrect.

--- config/915959680080171018/faqs/prefix.js
+++ config/915959680080171018/faqs/prefix.js
@@ -2,7 +2,7 @@ exports.answer = async client => ({
     title: `How do I find the XIV on Mac install directory?`,
     description: `The XIV on Mac install directory is a folder that contains important WINE configurations, the WINE`
         + `\n\nPrefix itself,as well as the Dalamud runtime, installed plugins, and game and system data for the XIV on Mac App.`
-        + `\n\nThe old prefix location is located at  \`~/Library/Application Support/XIV on Mac/wineprefix\``
+        + `\n\nThe XIV on Mac prefix is located at  \`~/Library/Application Support/XIV on Mac/wineprefix\``
         + `\n\nThe XIV on Mac install directory  is located at \`~/Library/Application Support/XIV on Mac\``
         + `\n\nFor more information, see `
         + `[HERE](https://www.xivmac.com/faq#q-prefix-folder?)`
