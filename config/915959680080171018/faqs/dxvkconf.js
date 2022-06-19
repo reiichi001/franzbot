512900e146e31fa316af8f6329c642ef475080be
Zoey Cluff <matada89@gmail.com>
2022-06-19 17:46:13 -0500

    change stuttering to tearing, I got bugs confused. There's nicer ways to tell me though, just FYI

--- config/915959680080171018/faqs/dxvkconf.js
+++ config/915959680080171018/faqs/dxvkconf.js
@@ -1,10 +1,10 @@
 exports.answer = async client => ({
-	title: `Stuttering after upgrading XOM to 4.0? Use this dxvk.conf file!`,
-	description: `A change in XOM 4.0 had the unintended affect of causing stuttering on some machines`
+	title: `Tearing after upgrading XOM to 4.0? Use this dxvk.conf file!`,
+	description: `A change in XOM 4.0 had the unintended affect of causing graphics tearing on some machines`
         + `\n\nThe dxvk.conf file linked below will resolve it`
         + `\n\nWith XIV On Mac focused hit cmd+i or go to file -> Open install location`
         + `\n\nand place the .conf file in wineprefix/drive_c`
-				+ `\n\nThis will resolve the stuttering`
+				+ `\n\nThis will resolve the tearing`
         + `\n\nClick the link below to download it! `
         + `[HERE](https://cdn.discordapp.com/attachments/915963559928811600/987158689574182982/dxvk.conf)`,
 
