8eea87b855fe239ae50e50924f3cf31b4150492b
Robert Baker <reiichi001@gmail.com>
2022-07-25 21:08:46 -0700

    Make ESLint shut up.

--- config/581875019861328007/faqs/vpn.js
+++ config/581875019861328007/faqs/vpn.js
@@ -2,8 +2,8 @@
 exports.answer = async client => ({
 	title: `SE's routing is shit. Use a VPN`,
 	description: `If you're getting consistent login or patch download failures on both XL and the official launcher, `
-		+ `it's likely a bad route from your connection. A VPN may help you bypass the issue.`
-		+ `[More info](https://goatcorp.github.io/faq/xl_troubleshooting#q-the-official-launcher-isnt-working--xivlauncher-failed-to-check-for-updates--patch-files-could-not-be-verified)`,
+		+ `it's likely a bad route from your connection. A VPN may help you bypass the issue. \n`
+		+ `More info [Here](https://goatcorp.github.io/faq/xl_troubleshooting#q-the-official-launcher-isnt-working--xivlauncher-failed-to-check-for-updates--patch-files-could-not-be-verified)`,
 	color: client.config.EMBED_NORMAL_COLOR,
 	footer: {
 		"text": client.config.FRANZBOT_VERSION,
--- config/915959680080171018/faqs/dalamudtesting.js
+++ config/915959680080171018/faqs/dalamudtesting.js
@@ -1,9 +1,14 @@
 exports.answer = async client => ({
-	title: `Only use Dalamud Testing when required`,
+	title: `How do I change my Dalamud branch? (Please only use Dalamud Testing when required)`,
 	description: `Please only enable Dalamud test builds when you intend to test Dalamud prereleases. `
-		+ `Expect crashes and failed launches and for it to ruin your raid night. More Info `
+		+ `They could be unstable, testing breaking features, or have bugs that the Dalamud dev team is working on. \n\n`
+		+ `To switch your Dalamud branch, you can use \`/xlbranch\` from in game, or edit your \`dalamudConfig.json\` file. `
+		+ `Expect crashes and failed launches on test builds and for it to ruin your raid night. \nMore Info `
 		+ `[HERE](https://goatcorp.github.io/faq/dalamud_troubleshooting#q-how-do-i-turn-dalamud-staging-on-or-off)`,
 	color: client.config.EMBED_NORMAL_COLOR,
+	image: {
+		"url": client.config.DALAMUDBRANCH,
+	},
 	footer: {
 		"text": client.config.FRANZBOT_VERSION,
 	},
