6668b1c1d646b06d2071db0ca3b22d9ad3a9421e
marzent <44967463+marzent@users.noreply.github.com>
2022-08-08 23:08:33 +0200

    reword dxvk config faq

--- config/915959680080171018/faqs/dxvkconf.js
+++ config/915959680080171018/faqs/dxvkconf.js
@@ -1,11 +1,11 @@
 exports.answer = async client => ({
-	title: `Tearing after upgrading XOM to 4.0? Use this dxvk.conf file!`,
-	description: `A change in XOM 4.0 had the unintended affect of causing graphics tearing on some machines`
-        + `\n\nThe dxvk.conf file linked below will resolve it`
+	title: `Experiencing tearing? Use this dxvk.conf file!`,
+	description: `Screen tearing is a relationship between framerate and display refresh where two different frames are being displayed. Any settings that manipulate that relationship could cause/resolve it - frame limiting/vsync are good examples`
+        + `\n\nThe dxvk.conf file linked below will force vsync and should resolve it`
         + `\n\nWith XIV On Mac focused hit cmd+i or go to file -> Open install location`
         + `\n\nand place the .conf file in wineprefix/drive_c`
 				+ `\n\nThis will resolve the tearing`
-        + `\n\nClick the link below to download it! `
+        + `\n\nClick the link here to download it! -->`
         + `[HERE](https://cdn.discordapp.com/attachments/915963559928811600/987158689574182982/dxvk.conf)`,
 
 	color: client.config.EMBED_NORMAL_COLOR,
