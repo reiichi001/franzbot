96ae72abe89eb1670caebe5c1af6d87a01892a26
Robert Baker <reiichi001@gmail.com>
2022-08-11 21:13:38 -0700

    Adjust remove config faq
    People keep typing `f!faq config`, so here.

--- config/581875019861328007/faqs/removeconfig.js
+++ config/581875019861328007/faqs/removeconfig.js
@@ -1,9 +1,9 @@
 exports.answer = async client => ({
-	title: `Generic steps to remove a plugin's config`,
-	description: `General "how to delete a plugin's config" steps:\n`
+	title: `Where can I find/delete a plugin's config?`,
+	description: ``
 		+ `1. Close the game and XIVLauncher\n`
 		+ `2. Go to \`%AppData%\\XIVLauncher\\pluginConfigs\`\n`
-		+ `3. Remove the offending config files\n`
+		+ `3. Remove the offending config file[s]\n`
 		+ `4. Start the game now\n`
 		+ `5. Let us know if the issue persists`,
 	color: client.config.EMBED_NORMAL_COLOR,
@@ -19,6 +19,7 @@ exports.info = {
 	name: "removeconfig",
 	category: "files",
 	aliases: [
+		"config",
 		"badconfig",
 		"deleteconfig",
 		"deletepluginconfig",
