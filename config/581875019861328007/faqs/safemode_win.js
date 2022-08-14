2d6ee208e640f82cbb396821cedfa3864b17d34f
tommadness <tom.m.cover@gmail.com>
2022-08-14 18:44:14 -0400

    Emphasize safemode clearing itself

--- config/581875019861328007/faqs/safemode_win.js
+++ config/581875019861328007/faqs/safemode_win.js
@@ -2,7 +2,7 @@ exports.answer = async client => ({
 	title: `How do I turn off Dalamud safe mode?`,
 	description: "Dalamud safemode occurs when an internal piece of the plugin system fails. "
 		+ "Usually, this indicates a hook failure or a previous Dalamud crash.\n\n"
-		+ "It should automatically clear when you try to relaunch. If it doesn't, head to "
+		+ "**It should automatically clear when you try to relaunch.** If it doesn't, head to "
 		+ "`%appdata%\\xivlauncher` and remove the `.dalamud_safemode` file in that folder.\n\n"
 		+ "NOTE: You may need to enable seeing hidden files.",
 	color: client.config.EMBED_NORMAL_COLOR,
