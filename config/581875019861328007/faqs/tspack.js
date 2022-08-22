c42e651f7f853720f02cfa546160bb6b2f0fccf6
NotNite <hi@notnite.com>
2022-08-21 21:04:55 -0400

    Add tspack faq

---
+++ config/581875019861328007/faqs/tspack.js
@@ -0,0 +1,32 @@
+exports.answer = async client => ({
+	title: `Please send us your .tspack for troubleshooting`,
+	description: `Please send us your troubleshooting pack from `
+		+ `\`%AppData%\\XIVLauncher\\\` in this channel, so we can look into the problem!`
+		+ `\n\nIt's best to just upload/attach the file if you can!`
+		+ `\n\nSteps to generate a troubleshooting pack:\n`
+		+ `1. Open XIVLauncher (hold shift if autologin is enabled)\n`
+		+ `2. Go to the XIVLauncher settings\n`
+		+ `3. Go to the "About" tab\n`
+		+ `4. Click the XIVLauncher logo`
+		+ `\n\n**NOTE**: If you have file extensions hidden, this file will just look like \`trouble-(some number)\` on your screen.`
+		+ `\n__Please upload the file directly. Even if you have Nitro, please make sure it's under 5.0 MB.__`,
+	color: client.config.EMBED_NORMAL_COLOR,
+	image: {
+		"url": client.config.WINDOWSEXPLORERSCREENSHOT,
+	},
+	footer: {
+		"text": client.config.FRANZBOT_VERSION,
+	},
+});
+
+exports.info = {
+	name: "logxl",
+	category: "logs",
+	aliases: [
+		"log",
+		"launcherlog",
+		"xivlauncherlog",
+		"logs",
+		"xllog",
+	],
+};
