9b5d916768fb4dcc41793655e43f449ed3520e22
Robert Baker <reiichi001@gmail.com>
2022-09-07 15:49:46 -0700

    Remember that windows backslashes are annoying

--- config/581875019861328007/faqs/repair.js
+++ config/581875019861328007/faqs/repair.js
@@ -4,7 +4,7 @@ exports.answer = async client => ({
 		+ `To repair a broken FFXIV installation, right-click on the login button and select \`Repair Game Files\`.\n\n`
 		+ `NOTE: Linux users will need to be using a prefix with Wine 7 or later. If you're on an older build,`
 		+ ` please make a new prefix and migrate first.\n\n`
-		+ `NOTE 2: All extra files that did not come with FFXIV will be moved to \`<gamepath>\\game\repair_recycle\`.  `
+		+ `NOTE 2: All extra files that did not come with FFXIV will be moved to \`<gamepath>\\game\\repair_recycle\`.  `
 		+ `This includes things like TexTools' mod db, GShade files and presets, graphics injectors, etc. You `
 		+ `will probably need to copy/move files back out or reinstall other addons again after repairing. `
 		+ `More Info: [HERE](https://goatcorp.github.io/faq/xl_troubleshooting#q-can-i-repair-my-ffxiv-installation)`,
