900758eeaca65eefbccdf0e97fe82cf5462a0887
Robert Baker <reiichi001@gmail.com>
2022-09-17 13:53:48 -0700

    Adjust some log handling
    - allow DM attachments up to 6MB
    - disable dalamud injector relay
    - silence tspack relay (for now)

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -150,7 +150,7 @@ module.exports = async (client, message) => {
 				console.log(attachment.name);
 				if (isDirectMessage && attachment.name.match(/(aria|output|dalamud|message|dalamudConfig|launcher|dxdiag|event|SquirrelSetup|patcher).*\.(log|txt|json|evtx)/gui)) {
 					// sane filesizes only
-					if (attachment.size > (5 * 1024 * 1024)) {
+					if (attachment.size > (6 * 1024 * 1024)) {
 						console.log("Big chonker file. That's a lot of text...");
 						message.channel.send("This file is pretty big and has not be relayed.\n"
 							+ "Perhaps you'd like to generate a fresh log that's smaller?\n\n"
@@ -195,7 +195,8 @@ module.exports = async (client, message) => {
 				}
 
 				// relay dalamud.injector.log files from goatplace (or DMs)
-				if (attachment.name.match(/dalamud\.injector.*\.(log)/gui)) {
+				const DALAMUD_INJECTOR_RELAY_ENABLE = false;
+				if (DALAMUD_INJECTOR_RELAY_ENABLE && attachment.name.match(/dalamud\.injector.*\.(log)/gui)) {
 					console.log(`Dalamud injector log upload: ${attachment.attachment}`);
 					// const response = await got(attachment.attachment);
 					console.log(`Fetched custom channel to relay: ${customChannel.name}`);
@@ -223,9 +224,9 @@ module.exports = async (client, message) => {
 						content: `${message.author.username} (${message.author}) uploaded a troubleshooting pack in ${isDirectMessage ? "DMs" : `${message.channel} from ${message.guild.name}`}.`,
 						files: [attachment],
 					});
-					await message.channel.send({
-						content: `Franzbot has relayed this file to a private channel in ${customChannel.guild.name} for analysis.\n`,
-						// `https://soulja-boy-told.me/loggy?url=${URLSafeBase64.encode(new Buffer.from(relayedMessage.attachments.first().url))}`
+					//await message.channel.send({
+					//	content: `Franzbot has relayed this file to a private channel in ${customChannel.guild.name} for analysis.\n`,
+						// `https://loggy.goat.place/?url=${URLSafeBase64.encode(new Buffer.from(relayedMessage.attachments.first().url))}`
 					});
 				}
 
