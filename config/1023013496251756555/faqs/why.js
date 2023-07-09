79340b840bda144b9e5232d6e7145095329e01f2
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-07-08 23:43:26 -0700

    Fix Mixed Spaces/Tabs

--- config/1023013496251756555/faqs/why.js
+++ config/1023013496251756555/faqs/why.js
@@ -4,10 +4,10 @@ exports.answer = async client => ({
 		+ `This leads to enormous amounts of wasted time and energy, `
 		+ `both on the part of people asking for help, and on the part of those providing help. `
 		+ `\nThe problem occurs when people get stuck on what they believe is the solution `
-  		+ `and are unable step back and explain the issue in full. `
+		+ `and are unable step back and explain the issue in full. `
 		+ `Please describe the overall task you are attempting to accomplish, `
 		+ `and we may be able to point you in a much better direction to solve your problem! `
-        	+ `\n\nMore information about The XY Problem can be found [HERE](https://xyproblem.info/). `,
+		+ `\n\nMore information about The XY Problem can be found [HERE](https://xyproblem.info/). `,
 	color: client.config.EMBED_NORMAL_COLOR,
 	footer: {
 		"text": client.config.FRANZBOT_VERSION,
