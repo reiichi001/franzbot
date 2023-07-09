9f0bfd5b1ef3a5ada9d96c88b714ab9331997a6b
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-07-08 23:42:29 -0700

    Fix Length Limits

--- config/1023013496251756555/faqs/why.js
+++ config/1023013496251756555/faqs/why.js
@@ -1,9 +1,12 @@
 exports.answer = async client => ({
 	title: `The XY Problem`,
 	description: `The XY problem is asking about your attempted solution rather than your actual problem. `
-		+ `This leads to enormous amounts of wasted time and energy, both on the part of people asking for help, and on the part of those providing help. `
-		+ `\nThe problem occurs when people get stuck on what they believe is the solution and are unable step back and explain the issue in full. `
-		+ `Please describe the overall task you are attempting to accomplish, and we may be able to point you in a much better direction to solve your problem! `
+		+ `This leads to enormous amounts of wasted time and energy, `
+		+ `both on the part of people asking for help, and on the part of those providing help. `
+		+ `\nThe problem occurs when people get stuck on what they believe is the solution `
+  		+ `and are unable step back and explain the issue in full. `
+		+ `Please describe the overall task you are attempting to accomplish, `
+		+ `and we may be able to point you in a much better direction to solve your problem! `
         	+ `\n\nMore information about The XY Problem can be found [HERE](https://xyproblem.info/). `,
 	color: client.config.EMBED_NORMAL_COLOR,
 	footer: {
