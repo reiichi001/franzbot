676dce7b9b8d81f6770e98db300a0a57d7cce6a0
Robert Baker <reiichi001@gmail.com>
2024-08-04 16:11:02 -0700

    Update plugintesting faq
    Also add it to Penumbra's server

--- config/915959680080171018/faqs/plugintesting.js
+++ config/915959680080171018/faqs/plugintesting.js
@@ -4,7 +4,8 @@ exports.answer = async client => ({
 		+ `1. Type \`/xlsettings\` in game.\n`
 		+ `2. Go to the \`Experimental\` tab\n`
 		+ `3. Click the checkbox for \`Get plugin testing builds\`\n`
-		+ `4. Save / Save and Close\n`
+		+ `4. Read the instructions on how to receive test builds for specific plugins.\n`
+		+ `5. Save / Save and Close\n`
 		+ `\n`
 		+ `**Please note that testing plugins can/will have bugs and may change dramatically before final release. `
 		+ `Especially on newer plugins, they could crash your game. `
