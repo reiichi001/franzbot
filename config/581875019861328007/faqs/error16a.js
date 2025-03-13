dd126bf7d1150cc33fb9bb9edf2e31877d032e56
Robert Baker <reiichi001@gmail.com>
2025-03-12 17:02:35 -0700

    Update asserts faq

---
+++ config/581875019861328007/faqs/error16a.js
@@ -0,0 +1,41 @@
+exports.answer = async client => ({
+	title: `FFXIV Error 16a`,
+	description: `FFXIV Error 16a means that the game config files are not readable. `
+		+ `There are multple causes for this issue.\n\n`
+		+ ``
+		+ `### File Permission Issues`
+		+ `**If you've been running FFXIV as admin, DON'T!\n\n`
+		+ ``
+		+ `Running FFXIV as admin can result in broken file permissions due to `
+		+ `a bug in FFXIV that will replace file and folder ownership. This `
+		+ `is not easily reversible. You will be responsible for fixing this on your own.\n\n`
+		+ ``
+		+ `### OneDrive sync issues\n`
+		+ `OneDrive may be trying to make your game config "online only," `
+		+ `which is not something that FFXIV knows how to handle correctly. `
+		+ `This can cause the game to crash.\n\n`
+		+ ``
+		+ `Please configure your FFXIV config folder to be available offline.\n`
+		+ `1. Go to your Documents folder.\n`
+		+ `2. Right-click the \`My Games\` folder.\n`
+		+ `3. Select the option for "Always keep on this device" or "Make available offline."\n`
+		+ `4. Wait a few minutes in case OneDrive has to redownload the files.\n`
+		+ `5. Try launching the game.`,
+	color: client.configdb.get("EMBED_NORMAL_COLOR"),
+	image: {
+		"url": "https://cdn.discordapp.com/attachments/687530726756712478/1349530670031962172/image.png",
+	},
+	footer: {
+		"text": client.configdb.get("FRANZBOT_VERSION"),
+	},
+});
+
+exports.info = {
+	name: "error16a",
+	category: "help",
+	aliases: [
+		"16a",
+		"16aerror",
+		"onedrive",
+	],
+};
--- config/915959680080171018/faqs/assert.js
+++ config/915959680080171018/faqs/assert.js
@@ -7,7 +7,14 @@ exports.answer = async client => ({
 		+ `When asserts are enabled, any plugin that breaks asserts can trigger the window, `
 		+ `not just the devplugin that enabled the feature.\n\n`
 		+ ``
-		+ `This is a developer-oriented feature.`,
+		+ `This is a developer-oriented feature.\n\n`
+		+ ``
+		+ `To disable assert checks:\n`
+		+ `1. Open Dalamud Settings (\`/xlsettings\`)\n`
+		+ `2. Click the Experimental tab\n`
+		+ `3. Scroll to the bottom of the Dev Plugins section\n`
+		+ `4. Uncheck the boxes for "Enable ImGui asserts" and "Always enable ImGui asserts at startup"\n`
+		+ `5. Save and relaunch the game.`,
 	color: client.configdb.get("EMBED_NORMAL_COLOR"),
 	image: {
 		"url": "https://cdn.discordapp.com/attachments/687530726756712478/1342982232997888030/image.png",
