721b5fddae7651c362dacbc923d71ffa518c29e1
Robert Baker <reiichi001@gmail.com>
2025-03-27 10:34:20 -0700

    Fix typos

--- config/884363610640498698/faqs/eventviewer.js
+++ config/884363610640498698/faqs/eventviewer.js
@@ -5,7 +5,7 @@ exports.answer = async client => ({
 		+ `\n\nYou can also run a search for \`ffxiv_dx11.exe\` in the "Find" action on the right-side panel.`
 		+ ` **Please send us all of the inner text in the General tab**. You can copy and paste it into a `
 		+ `\`\`\`fancy code block\`\`\` if you want, but a plain message is also fine.`
-		+ ` You can also copy it to a text file named \`event.log\` and DM it to Franzbot for pricacy.`
+		+ ` You can also copy it to a text file named \`event.log\` and DM it to Franzbot for privacy.`
 		+ `\n\nNOTE: You may have multiple crash events. Please let us know if your recent Event IDs are `
 		+ `stated as some for of CLR/.Net crash or if they're a native FFXIV error such as c000005.`,
 	color: client.configdb.get("EMBED_NORMAL_COLOR"),
