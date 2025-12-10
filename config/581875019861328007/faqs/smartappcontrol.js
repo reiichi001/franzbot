b08e3986760af399ea83f47afcb5904ae9372105
Robert Baker <reiichi001@gmail.com>
2025-12-10 14:29:51 -0800

    Update SAC faq

--- config/581875019861328007/faqs/smartappcontrol.js
+++ config/581875019861328007/faqs/smartappcontrol.js
@@ -1,17 +1,18 @@
 exports.answer = async client => ({
 	title: `Turn off Smart App Control`,
 	description: `Smart App Control is a newer feature introduced on Windows 10 `
-		+ `and is on by default for any brand new Windows install as of Windows 11.\n\n`
+		+ `and is on by default for any brand new Windows install as of Windows 11. `
+		+ `As of Windows 11 25H2, Microsoft has been turning it back on for existing installs.\n\n`
 		+ `Sometimes it can lead to blocking the XIVLauncher program/installer by asking you `
-		+ `to "Search for an app in the Store" to run XIVLauncher. (Which makes no sense given it's `
-		+ `its own application!)\n\n`
+		+ `to "Search for an app in the Store" to run XIVLauncher. Or may generate a warning claiming`
+		+ `that Dalamud (or dalamud.dll) may not be safe, as it is not codesigned.\n\n`
 		+ `**How to disable**\n`
 		+ `1. Open Windows Security\n`
 		+ `2. Go to "App & browser control\n`
 		+ `3. Open "Smart App Controll settings"\n`
 		+ `4. Switch it from "On" to "Evaluation" or "Off"\n\n`
-		+ `NOTE: You cannot turn this feature back on afterwards. Don't do this on a work machine or other `
-		+ `corporate/enterprise equipment or on a device you do not own/manage!`,
+		+ `NOTE: You may not be able to turn this feature back on afterwards. Don't do this on a `
+		+ `work machine or other corporate/enterprise equipment or on a device you do not own/manage!`,
 	color: client.configdb.get("EMBED_NORMAL_COLOR"),
 	image: {
 		"url": "https://cdn.discordapp.com/attachments/687530726756712478/1343066496712183860/e0dCR.png",
