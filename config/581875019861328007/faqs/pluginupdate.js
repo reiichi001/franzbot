54a2f8973fca440c7db43b25f6b5343b1a927e06
Ennea <Ennea@users.noreply.github.com>
2026-01-03 13:30:50 +0100

    add paragraph about alternatives and adoption link to pluginupdate FAQ entry

--- config/581875019861328007/faqs/pluginupdate.js
+++ config/581875019861328007/faqs/pluginupdate.js
@@ -2,7 +2,9 @@ exports.answer = async client => ({
 	title: `Please be patient and don't ask for plugin updates`,
 	description: `We do not know when a specific plugin will receive an update and do not have an ETA. `
 		+ `Only the developer of the plugin could possibly answer that. Please just wait for the plugin to be updated.\n\n`
-		+ `If a plugin does not get updated, other developers can adopt it after a certain period of time.`,
+		+ `Additionally, it is also worth mentioning that, no, there are no alternatives to that specific plugin you're waiting for. `
+		+ `There's a handful of plugins that do have some overlap with each other, but the majority of plugins do not.\n\n`
+		+ `If a plugin does not get updated, other developers can [adopt it](https://dalamud.dev/faq/adoption) after a certain period of time.`,
 	color: client.configdb.get("EMBED_NORMAL_COLOR"),
 	footer: {
 		"text": client.configdb.get("FRANZBOT_VERSION"),
