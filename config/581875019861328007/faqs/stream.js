dbf5354661a0a3784b247de51df6b90394285c2e
Robert Baker <reiichi001@gmail.com>
2026-01-10 12:45:38 -0800

    Update stream safety faq
    It wouldn't run on my test instance, but I think the syntax is correct. I guess I'll revert otherwise.

--- config/581875019861328007/faqs/stream.js
+++ config/581875019861328007/faqs/stream.js
@@ -1,18 +1,20 @@
 /* eslint-disable max-len */
 exports.answer = async client => ({
 	title: `Is it safe to stream with plugins?`,
-	description: `Generally, no. We highly advise that you disable all plugins or launch without plugins before streaming FFXIV.\n\n`
-		+ `However, there are a few ways to minimize your risks if you're going to stream anyways.\n`
-		+ `1. How are you recording? Are you capturing the entire screen or using a program that can capture only gameplay? `
-		+ `Choose the option to only capture gameplay via DirectX recording if possible.\n`
-		+ `2. If using OBS and similar, have you enabled "Hide Game UI" or related settings?\n`
-		+ `3. Are you using any other graphics injectors like Reshade, GShade, RivaTuner, or SpecialK? These can cause conflicts with the setting above.\n`
+	description: `Generally, no. We highly advise that you disable all plugins or launch without plugins before streaming FFXIV.\n`
 		+ `\n`
-		+ `However, even with all of the above precautions, there are several plugins that modify the native game interface and cannot be hidden. `
-		+ `Many plugins modify in-game user interface elements and are very good at it. You may not even realize a feature `
+		+ `**Is it still unsafe even if my capture software has a "Hide Game UI" feature?**\n`
+		+ `Put shortly? Yes\n\n`
+		+ `These features rely on caputuring from directx render layers using educated guesses about which layer is "before" game ui and other overlays. When you add additional layers on top of the game, like Dalamud, Reshade, etc, you introduce unexpected layers and it becoems more or less a gamble of whether it will work reliably or not.\n\n`
+		+ `You might be able to minimize your risks if you're going to stream anyways.\n`
+		+ `1. How are you recording? Are you capturing the entire screen or using a program that can capture only gameplay?`
+		+ `2. If using OBS and similar, have you enabled "Hide Game UI" or related settings? (These may not work reliably, as noted)\n`
+		+ `3. Other graphics injectors like Reshade, GShade, RivaTuner, SpecialK, or DLSSTweaks may conflict.\n`
+		+ `\n`
+		+ `Even with all of the above precautions, there are several plugins that modify the native game interface and cannot be hidden. `
+		+ `as they modify in-game user interface elements and are very good at it. You may not even realize a feature `
 		+ `is not actually part of the game because of this.\n\n`
-		+ `__You probably still don't want to stream with plugins enabled unless you're **absolutely** sure `
-		+ `that you have nothing that modifies in-game elements as they will always show up on your stream.__`,
+		+ `__You probably still don't want to stream with plugins enabled.__`,
 	color: client.configdb.get("EMBED_NORMAL_COLOR"),
 	footer: {
 		"text": client.configdb.get("FRANZBOT_VERSION"),
