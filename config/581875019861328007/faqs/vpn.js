4a77446b1bba430a09cce3dd04c41195929d600a
Phoosy <phoosy@phoosy.com>
2026-01-20 11:42:37 -0600

    this is so minor but just trying to make it as obvious as possible

--- config/581875019861328007/faqs/vpn.js
+++ config/581875019861328007/faqs/vpn.js
@@ -7,7 +7,7 @@ exports.answer = async client => ({
 		+ `Restart your network equipment \n`
 		+ `Change your DNS settings \n`
 		+ `**Using a VPN to help you bypass the issue.** \n\n`
-		+ `More specific info can be found [Here](https://goatcorp.github.io/faq/xl_troubleshooting#q-the-official-launcher-isnt-working--xivlauncher-failed-to-check-for-updates--patch-files-could-not-be-verified), `
+		+ `More specific info can be found [HERE](https://goatcorp.github.io/faq/xl_troubleshooting#q-the-official-launcher-isnt-working--xivlauncher-failed-to-check-for-updates--patch-files-could-not-be-verified), `
 		+ `including free VPN recommendations.`,
 	color: client.configdb.get("EMBED_NORMAL_COLOR"),
 	footer: {
