959d5868b87da5c94ed0a550999c060600c4ecf4
Robert Baker <reiichi001@gmail.com>
2026-02-07 12:37:34 -0800

    Break up Nvidia FAQ into separate topics

--- config/581875019861328007/faqs/legacynvidia.js
+++ config/581875019861328007/faqs/legacynvidia.js
@@ -1,6 +1,6 @@
 
 exports.answer = async client => ({
-	title: `Nvidia GPU Troubleshooting`,
+	title: `Legacy Nvidia GPU Troubleshooting`,
 	description: ``
 		+ `### Nvidia GTX 800/900/1000-series Cards\n`
 		+ `As of Nvidia RTX Driver Release 590 (December 2025), `
@@ -16,15 +16,6 @@ exports.answer = async client => ({
 		+ `\n`
 		+ `[Display Driver Uninstaller (DDU)](https://www.wagnardsoft.com/display-driver-uninstaller-ddu) `
 		+ `can be used to remove the current driver if you are having trouble rolling back.\n`
-		+ `\n`
-		+ `### Nvidia RTX 40/50-series Cards\n`
-		+ `Recent RTX GPUs have a feature called "Smooth Motion" that cause cause assorted issues.\n`
-		+ `- Crashing the game on launch\n`
-		+ `- Hanging on a black screen when the game launches\n`
-		+ `- Distored Dalamud/Plugin UI Windows\n`
-		+ `\n`
-		+ `It's recommended to turn this feature off for FFXIV using the Nvidia App. `
-		+ `We are looking into methods to get it to work correctly.`
 		+ ``,
 	color: client.configdb.get("EMBED_NORMAL_COLOR"),
 	footer: {
@@ -33,7 +24,7 @@ exports.answer = async client => ({
 });
 
 exports.info = {
-	name: "nvidia",
+	name: "legacynvidia",
 	category: "help",
 	aliases: [
 		"gtx800",
@@ -44,11 +35,15 @@ exports.info = {
 		"gtx1060",
 		"gtx1070",
 		"gtx1080",
-		"rtx40",
-		"rtx50",
-		"smoothmotion",
-		"legacynvidia",
+		"nvidia800",
+		"nvidia900",
+		"nvidia980",
+		"nvidia10",
+		"nvidia1000",
+		"nvidia1050",
+		"nvidia1060",
+		"nvidia1070",
+		"nvidia1080",
 		"nvidialegacy",
-		"nvidiadriver",
 	],
 };
---
+++ config/581875019861328007/faqs/smoothmotion.js
@@ -0,0 +1,28 @@
+
+exports.answer = async client => ({
+	title: `Modern Nvidia GPU Troubleshooting`,
+	description: ``
+		+ `### Nvidia RTX 40/50-series Cards\n`
+		+ `Recent RTX GPUs have a feature called "Smooth Motion" that cause cause assorted issues.\n`
+		+ `- Crashing the game on launch\n`
+		+ `- Hanging on a black screen when the game launches\n`
+		+ `- Distored Dalamud/Plugin UI Windows\n`
+		+ `\n`
+		+ `It's recommended to turn this feature off for FFXIV using the Nvidia App. `
+		+ `We are looking into methods to get it to work correctly.`
+		+ ``,
+	color: client.configdb.get("EMBED_NORMAL_COLOR"),
+	footer: {
+		"text": client.configdb.get("FRANZBOT_VERSION"),
+	},
+});
+
+exports.info = {
+	name: "smoothmotion",
+	category: "help",
+	aliases: [
+		"rtx40",
+		"rtx50",
+		"nvidiaapp",
+	],
+};
