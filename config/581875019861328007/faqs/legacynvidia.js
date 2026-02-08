b3e37bc192f0283e0c272145f40acca453580adf
Robert Baker <reiichi001@gmail.com>
2026-02-07 18:38:29 -0800

    Fix another typo
    If I earned a nickel every time I fixed one of my typos, maybe I bet I could pay off my student loans.

--- config/581875019861328007/faqs/legacynvidia.js
+++ config/581875019861328007/faqs/legacynvidia.js
@@ -9,7 +9,7 @@ exports.answer = async client => ({
 		+ `Release 580 versions.\n`
 		+ `\n`
 		+ ``
-		+ `**GeForce Security Update Driver 582.28** has known issues that can game crashes.\n`
+		+ `**GeForce Security Update Driver 582.28** has known issues that can cause game crashes.\n`
 		+ `\n`
 		+ `It is recommended to roll back to a prior version, such as [**Game Ready Driver 581.80**]`
 		+ `(https://www.nvidia.com/en-us/geforce/drivers/results/257496/).\n`
